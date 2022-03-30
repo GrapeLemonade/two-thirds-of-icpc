3d8aacdfd1d430a8aaf2f0ca641c5c3d692994c5
张羿凡 <1534438400@qq.com>
2022-03-30 10:52:33 +0800

    初始化 core.h 函数

--- core.h
+++ core.h
@@ -1,1 +1,9 @@
-#pragma once
+void init_words(char* words[], int len);
+
+void get_SCC();
+
+void check_loop();
+
+int get_all(char* result[]);
+
+int get_max(char* result[], char head, char tail, bool enable_loop, bool enable_self_loop);
