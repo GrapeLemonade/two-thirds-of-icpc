e53bb5ad2fdc9a3c2a9a911de0a673296a7658e0
张羿凡 <1534438400@qq.com>
2022-03-30 14:14:29 +0800

    完成函数 get_all, dfs_all, vector_to_result

--- core.cpp
+++ core.cpp
@@ -24,16 +24,15 @@ bool ins[26], vis[26];
 int dfn[26], low[26], cnt;
 int col[26], tot;
 
-
-void dfs(int i){
+void dfs_SCC(int i){
 	sk[++top] = i, vis[i] = ins[i] = true;
 	dfn[i] = low[i] = ++cnt;
-	for(auto x : v[i]){
-		int to = s[x].back() - 'a';
+	for(auto j : v[i]){
+		int to = s[j].back() - 'a';
 		if(vis[to]){ 
 			if(ins[to]) low[i] = min(low[i], low[to]);
 		}else{
-			dfs(to);
+			dfs_SCC(to);
 			low[i] = min(low[i], low[to]);
 		}
 	}
@@ -50,7 +49,7 @@ void dfs(int i){
 
 void get_SCC(){
 	for(int i = 0;i < m;i++) v[s[i][0] - 'a'].push_back(i);
-	for(int i = 0;i < n;i++) if(!vis[i]) dfs(i);
+	for(int i = 0;i < n;i++) if(!vis[i]) dfs_SCC(i);
 }
 
 void check_loop(){
@@ -59,7 +58,7 @@ void check_loop(){
 		for(auto j : v[i]){
 			if(s[j].back() - 'a' == i){
 				if(last == -1) last = j;
-				else throw invalid_argument("Word ring detected: " + s[last] + " " + s[j]);
+				else throw logic_error("Word ring detected: " + s[last] + " " + s[j]);
 			}
 		}
 	}
@@ -82,15 +81,54 @@ void check_loop(){
 						}
 					}
 				}
-				throw invalid_argument("Word ring detected: " + S);
+				throw logic_error("Word ring detected: " + S);
 			}
 			siz[col[i]] = true;
 		}
 	}
 }
 
+vector<string> ans;
+
+void vector_to_result(char* result[]){
+	int siz = 0;
+	for(auto &i : ans) siz += i.length() + 1;
+	char* p = (char*) malloc(siz);
+	for(int i = 0;i < ans.size();i++){
+		result[i] = p;
+		for(auto j : ans[i]){
+			*p = j;
+			p++;
+		}
+		*p = '\0';
+		p++;
+	}
+}
+
+vector<int> now;
+
+void dfs_all(int i, bool loop){
+	if(now.size() > 1){
+		string S;
+		for(auto j : now){
+			if(!S.empty()) S += " ";
+			S += s[j];
+		}
+		ans.push_back(S);
+	}
+	for(auto j : v[i]){
+		int to = s[j].back() - 'a';
+		if(i == to && loop) continue;
+		now.push_back(j);
+		dfs_all(to, i == to);
+		now.pop_back();
+	}
+}
+
 int get_all(char* result[]){
-		
+	for(int i = 0;i < 26;i++) dfs_all(i, false);
+	vector_to_result(result);
+	return now.size();
 }
 
 int get_max(char* result[], char head, char tail, bool enable_loop, bool enable_self_loop){
