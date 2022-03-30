009f5d2c89dae0c59dcbf80333e703041be6a7d3
张羿凡 <1534438400@qq.com>
2022-03-30 10:58:00 +0800

    完成函数 init_words

--- core.cpp
+++ core.cpp
@@ -1,21 +1,31 @@
 #include "core.h"
+#include <vector>
+#include <string>
 
-void init_words(char* words[], int len) {
+using namespace std;
+
+vector<string> s;
+
+void init_words(char* words[], int len){
+	for(int i = 0;i < len;i++){
+		string S = words[i];
+		if(S.length() == 1) continue; // 单个字母不算单词
+		s.push_back(S);
+	}
+}
+
+void get_SCC(){
 
 }
 
-void get_SCC() {
+void check_loop(){
 
 }
 
-void check_loop() {
+int get_all(char* result[]){
 
 }
 
-int get_all(char* result[]) {
-
-}
-
-int get_max(char* result[], char head, char tail, bool enable_loop, bool enable_self_loop) {
+int get_max(char* result[], char head, char tail, bool enable_loop, bool enable_self_loop){
 
 }
