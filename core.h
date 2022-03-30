8365fcc0cca24c8b078decf001132de71e849f36
张羿凡 <1534438400@qq.com>
2022-03-31 00:14:07 +0800

    修改函数 get_max_DAG 头文件中的参数

--- core.h
+++ core.h
@@ -6,4 +6,4 @@ void check_loop();
 
 int get_all(char* result[]);
 
-int get_max_DAG(char* result[], char head, char tail, bool enable_self_loop);
+int get_max_DAG(char* result[], char head, char tail, bool enable_self_loop, bool weighted);
