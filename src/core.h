db8733f52fa610e5dbe200d148f8cf194b04c32d
pantw <pantw@buaa.edu.cn>
2022-04-02 01:02:01 +0800

    将测试项目更名为 Test

--- src/core.h
+++ src/core.h
@@ -18,10 +18,14 @@ int engine(
 	bool enable_self_loop,
 	bool enable_ring);
 
+_declspec(dllexport)
 int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
 
+_declspec(dllexport)
 int gen_chains_all(char* words[], int len, char* result[]);
 
+_declspec(dllexport)
 int gen_chain_word_unique(char* words[], int len, char* result[]);
 
+_declspec(dllexport)
 int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
